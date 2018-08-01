298ad10e672045d76ca7a4a477edc22f7ebc9401
Divermv <Divermv@gmail.com>
2018-08-01 14:07:24 +0700

    Примерная последовательность

---
+++ main.c
@@ -0,0 +1,24 @@
+#include "head.h"
+
+int main (int argc, char *argv[])
+{
+    struct fields ships_fields;
+     char  opponent_addr[256];
+     int sock_id;
+
+	strcpy(opponent_addr,"");
+
+   sock_id=connect (opponent_addr);
+
+   //инициализируем окно ncurses
+      initscr();
+      start_color();
+      keypad (stdscr, TRUE);
+
+   ships_fields=reposition(ships_fields);// расставили корабли на своем поле
+
+    battle(ships_fields);
+
+    endwin();// разрушаем окно ncurses
+return 0;
+}
