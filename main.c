3a10d2d86f5ee6bdb5419111785904f9ad0b6fdb
Divermv <Divermv@gmail.com>
2018-08-01 14:26:13 +0700

    Версия без функций

--- main.c
+++ main.c
@@ -8,17 +8,17 @@ int main (int argc, char *argv[])
 
 	strcpy(opponent_addr,"");
 
-   sock_id=connect (opponent_addr);
+  // sock_id=connect (opponent_addr);
 
    //инициализируем окно ncurses
       initscr();
       start_color();
       keypad (stdscr, TRUE);
 
-   ships_fields=reposition(ships_fields);// расставили корабли на своем поле
+  // ships_fields=reposition(ships_fields);// расставили корабли на своем поле
 
-    battle(ships_fields);
-
+   // battle(ships_fields);
+sleep(2);
     endwin();// разрушаем окно ncurses
 return 0;
 }
