fc3382e8b0e0cd4cbfac3f707220a530e93a80af
Divermv <Divermv@gmail.com>
2018-08-02 10:58:22 +0700

    F wait_click

--- head.h
+++ head.h
@@ -16,13 +16,14 @@ struct fields{
 	char opponent_field[10][10];
 	char status[256];
 };
+struct 
 
 int connect_est (char *ip_port);                    //возвращает дескриптор, порт 8679
 
 struct fields reposition(struct fields field); // возвращает заполненную структуру
 
 int window (struct fields field);             //отрисовывает окна
-
+int *  wait_click(int win_number );// 0 - для своего окна, 1 - для окна противника
 int battle(struct fields field);              // возвращает исход боя
 
 #endif
