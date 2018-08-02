2676a1a0a3d979dc9d03fb28f6b07dac28a94999
Divermv <Divermv@gmail.com>
2018-08-02 11:54:30 +0700

    Help

--- main.c
+++ main.c
@@ -5,12 +5,19 @@ int main (int argc, char *argv[])
     struct fields ships_fields;
      char  opponent_addr[256];
      int sock_id;
-    if(argc>1)
+	
+     strcpy(opponent_addr,"");
+     if(argc>1)
 	for(int i=1;i<argc;i++)
+	{
 		printf("%s\n",argv[i]);
-sleep(2);
-	strcpy(opponent_addr,"");
-
+		if(!strcmp(argv[i],"-help")){
+		  printf("%s\n","Помощь спешит!");
+		return 0;
+		}
+		
+	}
+    sleep(2);
   // sock_id=connect (opponent_addr);
 
    //инициализируем окно ncurses
