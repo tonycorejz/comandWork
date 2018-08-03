722b7bfe6c5b29c4f87a412e48188787bf67c89b
Divermv <Divermv@gmail.com>
2018-08-03 12:24:08 +0700

    Проверка аргументов

--- main.c
+++ main.c
@@ -17,8 +17,9 @@ int main (int argc, char *argv[])
 		return 0;
 		}
 		if(!strcmp(argv[i],"-addr"))
-			if(!check_valid_addr[strcmp(argv[i+1])){
-		          printf("%s %s\n","IP адрес не соответсвует формату 192.168.2.138)");
+			//if(!check_valid_addr[strcmp(argv[i+1]))
+			{
+		          printf("%s %s\n",argv[i+1],"IP адрес не соответсвует формату 192.168.2.138");
 
 			}
 
