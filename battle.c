9d2af8e59a7a0922d031550f868555b447b7edcd
ImNia <agnia-kimi1996@mail.ru>
2018-08-07 11:16:45 +0700

    Добавлен waitClick

--- battle.c
+++ battle.c
@@ -23,9 +23,9 @@ int check(char opponent_field[10][10])
  * */
 int battle(struct fields field, int sock_fd, int stroke)
 {
-	int x, y;
     int repeat_move;
     int buf_coord[4];
+	int *b; //для получения координат от waitClick
     while(1){
 		/* Получаем от второго игрока координаты ячейки
 		 * отправляем второму игроку на проверку  
@@ -36,37 +36,35 @@ int battle(struct fields field, int sock_fd, int stroke)
         if(stroke == 1){ //игрок атакует
             repeat_move = 1;
             while(repeat_move == GOT_SHOT){
-	    	    scanf("%d %d", &x, &y); //функция вызова координат 
-		        buf_coord[0] = x;
-                buf_coord[1] = y;
+                b = wait_click(1);
+                buf_coord[0] = b[1];
+                buf_coord[1] = b[0];
                 send(sock_fd, buf_coord, sizeof(buf_coord), 0);
                 recv(sock_fd, buf_coord, sizeof(buf_coord), 0);
                 if(buf_coord[3] == 0)
                     break;
-                x = buf_coord[0];
-                y = buf_coord[1];
                 switch(buf_coord[2]){
     			    case 0:
-                        field.opponent_field[x][y] = 3;
+                        field.opponent_field[buf_coord[0]][buf_coord[1]] = 3;
 		    		    window(field);
                         repeat_move = 0;
 			        	break;
 
     			    case 1:
-		    	    	field.opponent_field[x][y] = 2;    
+		    	    	field.opponent_field[buf_coord[0]][buf_coord[1]] = 2;    
 		    		    window(field);
                         break;
 
 			        case 2:
-                        field.opponent_field[x][y] = 4;
+                        field.opponent_field[buf_coord[0]][buf_coord[1]] = 4;
 		    		    window(field);
-                        field.opponent_field[x][y] = 2;
+                        field.opponent_field[buf_coord[0]][buf_coord[1]] = 2;
 			    	    break;
 
     			    case 3:
-                        field.opponent_field[x][y] = 4;
+                        field.opponent_field[buf_coord[0]][buf_coord[1]] = 4;
 		    		    window(field);
-                        field.opponent_field[x][y] = 3;
+                        field.opponent_field[buf_coord[0]][buf_coord[1]] = 3;
 		    	    	break;
 				
 		        	default:
@@ -82,23 +80,21 @@ int battle(struct fields field, int sock_fd, int stroke)
             repeat_move = 1;
             while(repeat_move == GOT_SHOT){
                 recv(sock_fd, buf_coord, sizeof(buf_coord), 0);
-                x = buf_coord[0];
-                y = buf_coord[1];
-                switch(field.my_field[x][y]){
+                switch(field.my_field[buf_coord[0]][buf_coord[1]]){
                     case 0:
-				        field.my_field[x][y] = 3;
+				        field.my_field[buf_coord[0]][buf_coord[1]] = 3;
 		    		    window(field);
-                        buf_coord[0] = x;
-                        buf_coord[1] = y;
+                        buf_coord[0] = buf_coord[0];
+                        buf_coord[1] = buf_coord[1];
                         buf_coord[2] = 0;
 				        repeat_move = 0;
                         break;
 
 			        case 1:
-				        field.my_field[x][y] = 2;
+				        field.my_field[buf_coord[0]][buf_coord[1]] = 2;
 		    		    window(field);
-                        buf_coord[0] = x;
-                        buf_coord[1] = y;
+                        buf_coord[0] = buf_coord[0];
+                        buf_coord[1] = buf_coord[1];
                         buf_coord[2] = 1;
                         buf_coord[3] = check(field.my_field);
                         if(buf_coord[3] == 0)
